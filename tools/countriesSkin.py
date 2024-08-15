adae2126b7887d2c1e8e8fe079feafe0115c29d1
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-08-15 11:04:28 +0700

    Added France, Brazil, Finland, Japan, Australia, New Zealand skin color

--- tools/countriesSkin.py
+++ tools/countriesSkin.py
@@ -9,12 +9,18 @@ def start():
     ['singapore', singapore_skins],
     ['pakistan', pakistan_skins],
     ['bangladesh', bangladesh_skins],
+    ['france', france_skins],
+    ['finland', finland_skins],
+    ['brazil', brazil_skins],
+    ['japan', japan_skins],
+    ['australia', australia_skins],
+    ['new zealand', new_zealand_skins]
     ]
   
   countries_skin_list.sort()
   print()
   for option, skin_option in enumerate(countries_skin_list, start = 1):
-    print(f"{option}. {skin_option[0].capitalize()}")
+    print(f"{option}. {skin_option[0].title()}")
 
   global characters_length
   while True:
@@ -52,12 +58,12 @@ def start():
       print(f"Please select by number!");
 
 # =======================================================================
-
 def india_skins():
   print('y' * characters_length + 's' * int(characters_length / 2) + '4' * int(characters_length / 4) + 's' * int(characters_length / 2) + 'o' * characters_length)
 
 def indonesia_skins():
   print('5' * characters_length + 's' * characters_length)
+  print('2' * characters_length + 's' * characters_length)
 
 def malaysia_skins():
   print('s' * int(characters_length / 2) + '4' * int(characters_length / 2) + 'w' * int(characters_length / 2) + '4' * int(characters_length / 2) + ('s' * int(characters_length / 5) + '5' * int(characters_length / 5)) * 7)
@@ -73,3 +79,22 @@ def pakistan_skins():
 
 def bangladesh_skins():
   print('h' * int(characters_length / 2.5) + ',' * int(characters_length / 2) + 'h' * characters_length)
+
+def france_skins():
+  print('4' * characters_length + 's' * characters_length + '2' * characters_length)
+
+def finland_skins():
+  print('s' * characters_length + '4' * int(characters_length / 5) + 's' * int(characters_length / 5) + '4' * characters_length)
+
+def brazil_skins():
+  print('o' * int(characters_length / 1.5) + 'w' * int(characters_length / 2) + '4' * int(characters_length / 2) + 's' * int(characters_length / 5) + 'w' * int(characters_length / 2))
+
+def japan_skins():
+  print('s' * characters_length + '5' * int(characters_length / 1.5))
+  print('s' * characters_length + '2' * int(characters_length / 1.5))
+
+def australia_skins():
+  print(',' * int(characters_length / 1.5) + ('4' * int(characters_length / 2) + 's' * int(characters_length / 5)) * 5 + '4' * characters_length)
+
+def new_zealand_skins():
+  print(',' * int(characters_length / 1.5) + ('4' * int(characters_length / 2) + ',' * int(characters_length / 5)) * 4 + '4' * characters_length)
